6ac3780262245349b5390606db3376080e38ac5d
ShinR4M3N <99230342+ShinR4M3N@users.noreply.github.com>
2023-05-23 11:59:50 +1200

    pep8

--- Maori_quiz_final_version.py
+++ Maori_quiz_final_version.py
@@ -1,11 +1,12 @@
-""" Final version of the program
-All the feedbacks from the end user has been included (Format, Adding round numbers)
+""" Final version of the program All the feedbacks from the end user has
+been included (Format, Adding round numbers)
 
 """
 import random
 
 
-# Yes no checker. It asks for an answer of either yes or no. When something else it entered, it outputs an error
+# Yes no checker. It asks for an answer of either yes or no. When something
+# else it entered, it outputs an error
 
 def yes_no(question_text: object) -> object:
     while True:
@@ -33,10 +34,13 @@ def yes_no(question_text: object) -> object:
 def instructions():
     print("**** How to play ****")
     print()
-    print("This is a Maori quiz game\nThere are 2 sets of quiz.\nQuiz One being Maori Numbers from 1 to 10"
-          " and Quiz Two being Maori Day of the week\nYou can select the questions by typing"
+    print("This is a Maori quiz game\nThere are 2 sets of quiz.\nQuiz One "
+          "being Maori Numbers from 1 to 10"
+          "and Quiz Two being Maori Day of the week\nYou can select the "
+          "questions by typing"
           " o or one for set 1 and t or two for set 2\n"
-          "You will gain 1 point from getting question correct and lose 1 when you get something wrong\n"
+          "You will gain 1 point from getting question correct and lose 1 "
+          "when you get something wrong\n"
           "Now good luck and have fun!")
     print()
 
@@ -58,7 +62,8 @@ def question(question_text):
         elif answer == "two" or answer == "t":
             answer = "two"
             return answer
-        # If they say "x", it displays "Thank you for using our program," and exits
+        # If they say "x", it displays "Thank you for using our program,
+        # " and exits
         elif answer == "x":
             return answer
 
@@ -66,8 +71,8 @@ def question(question_text):
         else:
             print("Please answer 'o' or 't' or press 'x' to exit program: ")
 
-# Generates Maori numbers from 1 to 10 randomly with no repetitive question. Runs until all the words has run out from
-# the list
+# Generates Maori numbers from 1 to 10 randomly with no repetitive question.
+# Runs until all the words has run out from the list
 
 
 def generate_question_1():
@@ -76,8 +81,9 @@ def generate_question_1():
     rounds = 1
 
     # 1nd list
-    maori_numbers = [["tahi", "1"], ["rua", "2"], ["toru", "3"], ["wha", "4"], ["rima", "5"],
-                     ["ono", "6"], ["whitu", "7"], ["waru", "8"], ["iwa", "9"], ["tekau", "10"]]
+    maori_numbers = [["tahi", "1"], ["rua", "2"], ["toru", "3"], ["wha", "4"],
+                     ["rima", "5"], ["ono", "6"], ["whitu", "7"],
+                     ["waru", "8"], ["iwa", "9"], ["tekau", "10"]]
 
     # Generates random question
     random.shuffle(maori_numbers)
@@ -96,7 +102,8 @@ def generate_question_1():
 
     print(f"Your score was {point} points")
 
-# 2nd quiz function where it generates Days of the week in Maori. It also runs until all the words has run out/
+# 2nd quiz function where it generates Days of the week in Maori. It also
+# runs until all the words has run out/
 
 
 def generate_question_2():
@@ -105,8 +112,10 @@ def generate_question_2():
     rounds = 1
 
     # 1nd list
-    maori_days = [["Rahina", "MONDAY"], ["Ratu", "TUESDAY"], ["Raapa", "WEDNESDAY"], ["Rapare", "THURSDAY"],
-                  ["Ramere", "FRIDAY"], ["Rahoroi", "SATURDAY"], ["Ratapu", "SUNDAY"]]
+    maori_days = [["Rahina", "MONDAY"], ["Ratu", "TUESDAY"],
+                  ["Raapa", "WEDNESDAY"], ["Rapare", "THURSDAY"],
+                  ["Ramere", "FRIDAY"], ["Rahoroi", "SATURDAY"],
+                  ["Ratapu", "SUNDAY"]]
 
     # Generates random question
     random.shuffle(maori_days)
@@ -139,17 +148,20 @@ def formatter(symbol, text):
 # main routine
 print(formatter("-", "Welcome to Maori Quiz!"))
 print()
-played_before = yes_no("Have you play this game before? Please enter 'yes or 'no': ")
+played_before = yes_no("Have you play this game before? Please enter 'yes or "
+                       "'no': ")
 # Displays instruction when answer equals no
 if played_before == "No":
     instructions()
-    start = yes_no("Are you ready to start? (type 'yes' to continue 'no' to exit): ")
+    start = yes_no("Are you ready to start? (type 'yes' to continue 'no' to "
+                   "exit): ")
 
 # When answer equals yes, it directs into the menu function called question
 else:
     start = "Yes"
 
-# The user can choose what to play by typing "o" or "t" or they can press x to exit
+# The user can choose what to play by typing "o" or "t" or they can press x
+# to exit
 while start == "Yes":
     question_no = question("What question would you like to play?"
                            "\nPress 'o' for question set 1"
